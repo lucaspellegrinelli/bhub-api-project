0fae9e38ed7f9862f3e50d5666c2fc9b2f533b07
Lucas Pellegrinelli <lucas.r.pellegrinelli@gmail.com>
2022-11-09 23:28:39 -0300

    Added Mock database fixture for functional tests

---
+++ tests/conftest.py
@@ -0,0 +1,55 @@
+from typing import Generator, Any
+
+import pytest
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi.testclient import TestClient
+
+from core.sqlalchemy import Base, get_db
+
+from apis.users import router as users_router
+
+SQL_ALCHEMY_TESTING_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQL_ALCHEMY_TESTING_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def start_application() -> FastAPI:
+    app = FastAPI()
+    app.include_router(users_router)
+    return app
+
+@pytest.fixture(scope="function")
+def app() -> Generator[FastAPI, Any, None]:
+    """ Create a fresh database on each test case. """
+    Base.metadata.create_all(engine) # Create the tables.
+    _app = start_application()
+    yield _app
+    Base.metadata.drop_all(engine)
+
+@pytest.fixture(scope="function")
+def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = SessionTesting(bind=connection)
+    yield session # use the session in tests.
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+@pytest.fixture(scope="function")
+def client(app: FastAPI, db_session: SessionTesting) -> Generator[TestClient, Any, None]:
+    """
+    Create a new FastAPI TestClient that uses the `db_session` fixture to override
+    the `get_db` dependency that is injected into routes.
+    """
+
+    def _get_test_db():
+        try:
+            yield db_session
+        finally:
+            pass
+    
+    app.dependency_overrides[get_db] = _get_test_db
+    with TestClient(app) as client:
+        yield client
